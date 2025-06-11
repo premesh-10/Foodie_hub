cb0d505ebcdee2a39351f8ad204ddba51638d59d
premesh-10 <166977910+premesh-10@users.noreply.github.com>
2025-06-11 21:51:40 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer} from 'react-toastify';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://foodie-hub-backend-wwvi.onrender.com"
   return (
     <div>
       <ToastContainer/>
